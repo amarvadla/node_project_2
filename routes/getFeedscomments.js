9bd82bf979c38cee87c390ebc0bb054f7480be0c
amarnath <avadla@gemini-us.com>
2019-08-19 18:39:09 +0530

    aggregate a collection

--- routes/getFeedscomments.js
+++ routes/getFeedscomments.js
@@ -2,6 +2,7 @@ var express = require('express')
 var router = express.Router()
 var feeds = require('../schemas/feeds')
 var userSchema = require('../schemas/userSchema')
+var ObjectId = require('mongodb').ObjectID;
 
 router.get('/', (req, res) => {
     var input = req.query
@@ -10,30 +11,51 @@ router.get('/', (req, res) => {
         if (err) {
             res.json({ statusCode: 0, statusMessage: 'wrong user id' })
         } else if (data) {
-            feeds.findById({ _id: input.feedId }, (err, dataObj) => {
-                if (err) {
-                    res.json({ statusCode: 0, statusMessage: 'wrong feed id' })
-                } else if (dataObj) {
+            // feeds.findById({ _id: input.feedId }, (err, dataObj) => {
+            //     if (err) {
+            //         res.json({ statusCode: 0, statusMessage: 'wrong feed id' })
+            //     } else if (dataObj) {
 
-                    var commentsArray = []
-                    for (var i = 0; i < dataObj.comments.length; i++) {
-                        var resObj = {}
-                        resObj.id = dataObj.comments[i]._id
-                        resObj.comment = dataObj.comments[i].comment
-                        userSchema.findById({ _id: dataObj.comments[i].userId }, (err, data) => {
-                            resObj.username = data.username
-                        })
 
-                        commentsArray.push(resObj)
-                    }
+            //     }
+            // })
 
-                    res.json({
-                        statusCode: 1,
-                        statusMessage: 'success',
-                        data: commentsArray
-                    })
+
+            feeds.aggregate([{ $match: { _id: ObjectId("5d4bcf9126bc8328dc3f9af5") } },
+             { "$unwind": "$comments" },
+             { $replaceRoot: { newRoot: "$comments" } },
+            { $lookup: { from: "userschemas", localField: "userId", foreignField: "_id", as: "userObj" } },
+            { "$unwind": "$userObj" },
+            {
+                $project: {
+                    commentId: "$_id", _id: 0, comment: 1, userId: 1,
+                    profileImage: "$userObj.password"
                 }
+            }
+            ]).exec(function (err, data) {
+                // if (err) {
+                //     console.log(err)
+                // }
+                // var dataObj = data[0]
+                // var commentsArray = []
+                // for (var i = 0; i < dataObj.comments.length; i++) {
+                //     var resObj = {}
+                //     resObj.id = dataObj.comments[i]._id
+                //     resObj.comment = dataObj.comments[i].comment
+                //     resObj.username = dataObj.userObj.username
+
+                //     commentsArray.push(resObj)
+                // }
+
+
+                res.json({
+                    statusCode: 1,
+                    statusMessage: 'success',
+                    data: data
+                })
             })
+
+
         }
     })
 
