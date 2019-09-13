712a833419fd4e48210a45065b79c7b014e24ec1
amarv <amarvadla27@gmail.com>
2019-09-13 13:54:35 +0530

    date changes and update

--- routes/publishQuestion.js
+++ routes/publishQuestion.js
@@ -7,13 +7,36 @@ router.get('/', (req, res) => {
     var input = req.query;
 
     liveStreamSchema.aggregate([
-        { $match :{ _id : ObjectId(input.contestId)}},
-        {"$unwind" : "$questions"},
-        {$replaceRoot: { newRoot: "$questions" } },
-        { $match : {_id : ObjectId(input.questionId)}}
-        ]).exec( (err,data) =>{
-            res.json(data)
-        })
+        { $match: { _id: ObjectId(input.contestId) } },
+        { "$unwind": "$questions" },
+        { $replaceRoot: { newRoot: "$questions" } },
+        { $match: { _id: ObjectId(input.questionId) } }
+    ]).exec((err, data) => {
+
+        if (data) {
+
+            var ques = data[0];
+
+            questionTime = ques.questionTime;
+
+            var currentDat = new Date();
+            var changedDate = new Date();
+            changedDate.setSeconds(changedDate.getSeconds() + questionTime);
+
+
+            liveStreamSchema.update({
+                _id: ObjectId(input.contestId),
+                questions: { $elemMatch: { _id: ObjectId(input.questionId) } }
+            }, { $set: { "questions.$.startDate": new Date(), "questions.$.endDate": changedDate } }, (err, data) => {
+                if (err) {
+                    res.json(err)
+                } else if (data) {
+                    res.json(data)
+                }
+            })
+        }
+
+    })
 
 })
 
